b5aba6f569b3ccf09d14c9ab01ab6ec5816ab1a5
FlyingAnt <liu.cheng@breezecapsule.com>
2013-07-31 15:24:34 +0800

    first commit

---
+++ js/HeroCollection.js
@@ -0,0 +1,575 @@
+var HeroCollection = Backbone.Collection.extend({
+
+    initialize: function(){
+        this.add([
+            {
+                name: "于吉",
+                url: "source/于吉.png"
+            },
+
+
+            {
+                name: "于禁",
+                url: "source/于禁.png"
+            },
+
+
+            {
+                name: "关兴&张苞",
+                url: "source/关兴&张苞.jpg"
+            },
+
+
+            {
+                name: "关羽",
+                url: "source/关羽.png"
+            },
+
+
+            {
+                name: "典韦",
+                url: "source/典韦.png"
+            },
+
+
+            {
+                name: "凌统",
+                url: "source/凌统.png"
+            },
+
+
+            {
+                name: "刘备",
+                url: "source/刘备.png"
+            },
+
+
+            {
+                name: "刘禅",
+                url: "source/刘禅.png"
+            },
+
+
+            {
+                name: "刘表",
+                url: "source/刘表.jpg"
+            },
+
+
+            {
+                name: "华佗",
+                url: "source/华佗.png"
+            },
+
+
+            {
+                name: "华雄",
+                url: "source/华雄.jpg"
+            },
+
+
+            {
+                name: "司马懿",
+                url: "source/司马懿.png"
+            },
+
+
+            {
+                name: "吕布",
+                url: "source/吕布.png"
+            },
+
+
+            {
+                name: "吕蒙",
+                url: "source/吕蒙.png"
+            },
+
+
+            {
+                name: "吴国太",
+                url: "source/吴国太.png"
+            },
+
+
+            {
+                name: "周泰",
+                url: "source/周泰.png"
+            },
+
+
+            {
+                name: "周瑜",
+                url: "source/周瑜.png"
+            },
+
+
+            {
+                name: "夏侯惇",
+                url: "source/夏侯惇.png"
+            },
+
+
+            {
+                name: "夏侯渊",
+                url: "source/夏侯渊.png"
+            },
+
+
+            {
+                name: "大乔",
+                url: "source/大乔.png"
+            },
+
+
+            {
+                name: "太史慈",
+                url: "source/太史慈.png"
+            },
+
+
+            {
+                name: "姜维",
+                url: "source/姜维.png"
+            },
+
+
+            {
+                name: "孙坚",
+                url: "source/孙坚.png"
+            },
+
+
+            {
+                name: "孙尚香",
+                url: "source/孙尚香.png"
+            },
+
+
+            {
+                name: "孙权",
+                url: "source/孙权.png"
+            },
+
+
+            {
+                name: "孙策",
+                url: "source/孙策.png"
+            },
+
+
+            {
+                name: "孟获",
+                url: "source/孟获.png"
+            },
+
+
+            {
+                name: "小乔",
+                url: "source/小乔.png"
+            },
+
+
+            {
+                name: "左慈",
+                url: "source/左慈.png"
+            },
+
+
+            {
+                name: "庞德",
+                url: "source/庞德.png"
+            },
+
+
+            {
+                name: "庞统",
+                url: "source/庞统.png"
+            },
+
+
+            {
+                name: "廖化",
+                url: "source/廖化.jpg"
+            },
+
+
+            {
+                name: "张昭&张纮",
+                url: "source/张昭&张纮.png"
+            },
+
+
+            {
+                name: "张春华",
+                url: "source/张春华.png"
+            },
+
+
+            {
+                name: "张角",
+                url: "source/张角.png"
+            },
+
+
+            {
+                name: "张辽",
+                url: "source/张辽.png"
+            },
+
+
+            {
+                name: "张颌",
+                url: "source/张颌.png"
+            },
+
+
+            {
+                name: "张飞",
+                url: "source/张飞.png"
+            },
+
+
+            {
+                name: "徐庶",
+                url: "source/徐庶.png"
+            },
+
+
+            {
+                name: "徐晃",
+                url: "source/徐晃.png"
+            },
+
+
+            {
+                name: "徐盛",
+                url: "source/徐盛.png"
+            },
+
+
+            {
+                name: "曹丕",
+                url: "source/曹丕.png"
+            },
+
+
+            {
+                name: "曹仁",
+                url: "source/曹仁.png"
+            },
+
+
+            {
+                name: "曹彰",
+                url: "source/曹彰.jpg"
+            },
+
+
+            {
+                name: "曹操",
+                url: "source/曹操.png"
+            },
+
+
+            {
+                name: "曹植",
+                url: "source/曹植.png"
+            },
+
+
+            {
+                name: "步练师",
+                url: "source/步练师.jpg"
+            },
+
+
+            {
+                name: "法正",
+                url: "source/法正.png"
+            },
+
+
+            {
+                name: "火诸葛亮",
+                url: "source/火诸葛亮.png"
+            },
+
+
+            {
+                name: "王异",
+                url: "source/王异.jpg"
+            },
+
+
+            {
+                name: "甄姬",
+                url: "source/甄姬.png"
+            },
+
+
+            {
+                name: "甘宁",
+                url: "source/甘宁.png"
+            },
+
+
+            {
+                name: "祝融",
+                url: "source/祝融.png"
+            },
+
+
+            {
+                name: "神关羽",
+                url: "source/神关羽.png"
+            },
+
+
+            {
+                name: "神司马懿",
+                url: "source/神司马懿.png"
+            },
+
+
+            {
+                name: "神吕布",
+                url: "source/神吕布.png"
+            },
+
+
+            {
+                name: "神吕蒙",
+                url: "source/神吕蒙.png"
+            },
+
+
+            {
+                name: "神周瑜",
+                url: "source/神周瑜.png"
+            },
+
+
+            {
+                name: "神曹操",
+                url: "source/神曹操.png"
+            },
+
+
+            {
+                name: "神诸葛亮",
+                url: "source/神诸葛亮.png"
+            },
+
+
+            {
+                name: "神赵云",
+                url: "source/神赵云.png"
+            },
+
+
+            {
+                name: "程普",
+                url: "source/程普.jpg"
+            },
+
+
+            {
+                name: "荀彧",
+                url: "source/荀彧.png"
+            },
+
+
+            {
+                name: "荀攸",
+                url: "source/荀攸.jpg"
+            },
+
+
+            {
+                name: "董卓",
+                url: "source/董卓.png"
+            },
+
+
+            {
+                name: "蔡文姬",
+                url: "source/蔡文姬.png"
+            },
+
+
+            {
+                name: "袁绍",
+                url: "source/袁绍.png"
+            },
+
+
+            {
+                name: "许褚",
+                url: "source/许褚.png"
+            },
+
+
+            {
+                name: "诸葛亮",
+                url: "source/诸葛亮.png"
+            },
+
+
+            {
+                name: "貂蝉",
+                url: "source/貂蝉.png"
+            },
+
+
+            {
+                name: "贾诩",
+                url: "source/贾诩.png"
+            },
+
+
+            {
+                name: "赵云",
+                url: "source/赵云.png"
+            },
+
+
+            {
+                name: "邓艾",
+                url: "source/邓艾.png"
+            },
+
+
+            {
+                name: "郭嘉",
+                url: "source/郭嘉.png"
+            },
+
+
+            {
+                name: "钟会",
+                url: "source/钟会.jpg"
+            },
+
+
+            {
+                name: "陆逊",
+                url: "source/陆逊.png"
+            },
+
+
+            {
+                name: "陈宫",
+                url: "source/陈宫.png"
+            },
+
+
+            {
+                name: "韩当",
+                url: "source/韩当.jpg"
+            },
+
+
+            {
+                name: "颜良&文丑",
+                url: "source/颜良&文丑.png"
+            },
+
+
+            {
+                name: "马岱",
+                url: "source/马岱.jpg"
+            },
+
+
+            {
+                name: "马谡",
+                url: "source/马谡.png"
+            },
+
+
+            {
+                name: "马超",
+                url: "source/马超.png"
+            },
+
+
+            {
+                name: "高顺",
+                url: "source/高顺.png"
+            },
+
+
+            {
+                name: "魏延",
+                url: "source/魏延.png"
+            },
+
+
+            {
+                name: "鲁肃",
+                url: "source/鲁肃.png"
+            },
+
+
+            {
+                name: "黄忠",
+                url: "source/黄忠.png"
+            },
+
+
+            {
+                name: "黄月英",
+                url: "source/黄月英.png"
+            },
+
+
+            {
+                name: "黄盖",
+                url: "source/黄盖.png"
+            }
+        ]);
+    },
+
+    filterMasterHeros: function(){
+        return [
+            {
+                name: "刘备",
+                url: "source/刘备.png"
+            },
+            {
+                name: "刘禅",
+                url: "source/刘禅.png"
+            },
+            {
+                name: "曹操",
+                url: "source/曹操.png"
+            },
+            {
+                name: "曹丕",
+                url: "source/曹丕.png"
+            },
+            {
+                name: "孙权",
+                url: "source/孙权.png"
+            },
+            {
+                name: "孙策",
+                url: "source/孙策.png"
+            },
+            {
+                name: "张角",
+                url: "source/张角.png"
+            },
+            {
+                name: "袁绍",
+                url: "source/袁绍.png"
+            },
+            {
+                name: "董卓",
+                url: "source/董卓.png"
+            }
+        ]
+    }
+
+})
---
+++ js/HeroSelectView.js
@@ -0,0 +1,126 @@
+var HeroSelectView = Backbone.View.extend({
+
+    events:{
+        "click a[data-action=list]": "_listAll",
+        "click a[data-action=selectMaster]": "_filterWithMaster",
+        "click a[data-action=select]": "_filter",
+        "click .item.active":"_select",
+        "click a[data-action=search]": "_search",
+        "click a[data-action=back]": "_back"
+    },
+
+    template: function(){
+        return Handlebars.compile($("#heroItemsTemplate").html())
+    },
+
+    initialize: function(){
+        console.log("Initialize ... Hero size",this.collection.size());
+        this.on("render", this.render, this);
+    },
+
+    render: function(){
+        var self = this;
+        this.$(".carousel-inner").html(self.template()({
+            heros: self.filterHeros
+        }));
+        this.$(".carousel-inner div:first-child").addClass("active");
+        $("#viewHerosPanle").carousel({
+            interval: 2000000
+        });
+        this._hideOperations();
+        this._showHeroZone();
+    },
+
+    _hideOperations: function(){
+        this.$(".operations").hide();
+    },
+
+    displayOperations: function(){
+        this.$(".operations").show();
+    },
+
+    _filter: function(){
+        this.filterHeros = new Array();
+        for(var i=0;i<5;i++){
+            this.filterHeros.push(this.filterOne());
+        }
+        this.trigger("render");
+    },
+
+    _listAll: function(){
+        this.filterHeros = new Array();
+        this.filterHeros = _.shuffle(this.collection.toJSON());
+        this.trigger("render");
+    },
+
+    _search: function(){
+        var self = this;
+        var searchString = this.$("input[type=text]").val();
+        if(!searchString){
+            alert("点你妹啊！");
+            return
+        }
+        this.filterHeros = new Array();
+        var hero = _.find(self.collection.toJSON(),function(h){
+            return h.name === searchString
+        });
+        if(hero){
+            this.filterHeros.push(hero);
+            this.trigger("render");
+        } else {
+            alert("你确定有这货！");
+            return
+        }
+    },
+
+    _filterWithMaster: function(){
+        var self = this;
+        this.filterHeros = new Array();
+        this.filterHeros = this.collection.filterMasterHeros();
+        for(var i=0;i<5;i++){
+            this.filterHeros.push(self.filterOne());
+        }
+        this.trigger("render");
+    },
+
+    filterOne: function(){
+        var self = this;
+        while(true){
+            var hero = _.first(self.collection.shuffle()).toJSON();
+            if(_.find(self.filterHeros, function(h){
+                return h.name === hero.name
+            }) === undefined ){
+                return hero;
+            }
+        };
+    },
+
+    _hideHeroZone: function(){
+        this.$(".heroZone").hide();
+    },
+
+    _showHeroZone: function(){
+        this.$(".heroZone").show();
+    },
+
+    _hideViewHero: function(){
+        this.$(".viewHero").hide();
+    },
+
+    _showViewHero: function(){
+        this.$(".viewHero").show();
+    },
+
+    _select: function(event){
+        var heroUrl = $(event.currentTarget).data("url");
+        this.$(".viewHero").html("<img class='selectedHero' src='"+heroUrl+"'></img><a class='btn btn-danger' data-action='back'>BBBBBBBBBBBACK</a>");
+        this._hideOperations();
+        this._hideHeroZone();
+        this._showViewHero();
+    },
+
+    _back: function(){
+        this._hideViewHero();
+        this.displayOperations();
+    }
+})
